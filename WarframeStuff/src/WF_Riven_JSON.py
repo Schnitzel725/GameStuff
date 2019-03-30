6c35aa33014bac30de3f337206e3bcdceafb53b4
Schnitzel725 <ruizhe1997@gmail.com>
2019-03-29 22:17:06 -0500

    Update WF_Riven_JSON.py

--- WarframeStuff/src/WF_Riven_JSON.py
+++ WarframeStuff/src/WF_Riven_JSON.py
@@ -8,7 +8,7 @@ xb = pd.read_json('http://n9e5v4d8.ssl.hwcdn.net/repos/weeklyRivensXB1.json', or
 # converts the dataframe's columns to something understandable
 def json_convert(df, platform):
     df = df[['itemType', 'compatibility', 'rerolled', 'pop', 'min', 'max', 'avg', 'stddev']]
-    df.columns = ['Item Type', 'For Weapon', 'Rerolled', 'Popularity', 'Lowest Price (Plat)', 'Highest Price (Plat)', 'Average Trade Value', 'Average Price Variation']
+    df.columns = ['Item Type', 'For Weapon', 'Rerolled', 'Popularity', 'Lowest Price (Platinum)', 'Highest Price (Platinum)', 'Average Trade Value', 'Average Price Variation']
     file_name = platform + '_json_data.xlsx'
     df.to_excel(file_name, sheet_name='PC')
 
